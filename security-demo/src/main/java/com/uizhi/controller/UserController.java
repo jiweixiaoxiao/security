656d33712ed32146eefa9173b3ed6091660aaae7
jiwei <4309473@QQ.COM>
2017-10-22 01:44:47 +0800

    restful的增删改查

--- security-demo/src/main/java/com/uizhi/controller/UserController.java
+++ security-demo/src/main/java/com/uizhi/controller/UserController.java
@@ -2,9 +2,13 @@ package com.uizhi.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.uizhi.dto.User;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,10 +19,11 @@ import java.util.List;
  * Time: 20:33
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping
     @JsonView(User.UserSimpleView.class)
-    public List<User> query(){
+    public List<User> query() {
         List<User> user = new ArrayList<>();
         user.add(new User());
         user.add(new User());
@@ -26,11 +31,50 @@ public class UserController {
         return user;
     }
 
-    @RequestMapping(value = "/user/{id:\\d+}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id:\\d+}")
     @JsonView(User.UserDetailView.class)
-    public User getInfo(@PathVariable String id){
+    public User getInfo(@PathVariable String id) {
         User user = new User();
         user.setUserName("lisi");
         return user;
+    }
+
+    @PostMapping
+    public User create(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().stream().forEach(objectError -> System.out.println(objectError.getDefaultMessage()));
+        }
+        System.out.println("userid=[" + user.getId() + "]");
+        System.out.println("username = [" + user.getUserName() + "]");
+        System.out.println("userpassword = [" + user.getPassword() + "]");
+        System.out.println("userBirthday=[" + user.getBirthDay() + "]");
+        user.setId(1);
+        return user;
+    }
+
+    @PutMapping("/{id:\\d+}")
+    public User update(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().stream().forEach(objectError -> {
+//                        FieldError fieldError = (FieldError) objectError;
+//                        String message = fieldError.getField() + " " + fieldError.getDefaultMessage();
+//                        System.out.println(message);
+                        System.out.println(objectError.getDefaultMessage());
+                    }
+            );
+        }
+        System.out.println("userid=[" + user.getId() + "]");
+        System.out.println("username = [" + user.getUserName() + "]");
+        System.out.println("userpassword = [" + user.getPassword() + "]");
+        System.out.println("userBirthday=[" + user.getBirthDay() + "]");
+        user.setId(1);
+        return user;
+
+    }
+
+
+    @DeleteMapping("/{id:\\d+}")
+    public void delete(@PathVariable String id){
+        System.out.println(id);
     }
 }
--- security-demo/src/main/java/com/uizhi/dto/User.java
+++ security-demo/src/main/java/com/uizhi/dto/User.java
@@ -2,6 +2,9 @@ package com.uizhi.dto;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,8 +19,30 @@ public class User {
     //1 用接口来声明多个视图
     public interface UserSimpleView{};
     public interface UserDetailView extends UserSimpleView{};
+    private int id;
     private String userName;
+    @NotBlank(message = "亲，密码不能为空")
     private String password;
+    private Date birthDay;
+
+    @JsonView(UserSimpleView.class)
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+
+    @JsonView(UserSimpleView.class)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     @JsonView(UserSimpleView.class)
     public String getUserName() {
